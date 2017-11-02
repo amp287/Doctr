a30780956607026d288978e816ad4848f618a85c
DESKTOP-IKCSH5V\mosborn1987 <mosborn1987@gmail.com>
2017-11-02 14:27:05 -0400

    Added Birth Date to spinner

--- app/src/main/java/com/group/poop/doctr/Register.java
+++ app/src/main/java/com/group/poop/doctr/Register.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import java.util.List;
 
 public class Register extends AppCompatActivity {
 
@@ -17,6 +21,9 @@ public class Register extends AppCompatActivity {
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private Spinner mMonthSpinner;
+    private Spinner mDateSpinner;
+
     private DataBase dataBase;
 
     @Override
@@ -29,6 +36,11 @@ public class Register extends AppCompatActivity {
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         mPasswordView = (EditText) findViewById(R.id.password);
+        mMonthSpinner = (Spinner) findViewById(R.id.month_spinner);
+        mDateSpinner = (Spinner) findViewById(R.id.date_spinner);
+
+        // Setup
+        addItemsToDateSpinner(31);
 
         // Get The email address from Login Form
         String passedEmail;
@@ -48,11 +60,35 @@ public class Register extends AppCompatActivity {
 
     }
 
+    public void addItemsToDateSpinner(int days) {
+
+        if( (days > 31) || (days <= 0) )
+        {
+            // TODO - Handle this with and Exception
+            // Set default
+            days = 31;
+        }
+        List<String> daysInMonthList = new ArrayList<String>();
+
+        daysInMonthList.add("Select Day");
+        for(int i = 1; i <= days; i++)
+        {
+            daysInMonthList.add(Integer.toString(i));
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, daysInMonthList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mDateSpinner.setAdapter(dataAdapter);
+    }
+
     public void onGenderRadioButtonClicked(View view)
     {
         // TODO - Not sure if anything needs to be done here.
 
     }
+
+    // TODO - Create a spinner Listener Function for the month and date spinner
 
 }
 
