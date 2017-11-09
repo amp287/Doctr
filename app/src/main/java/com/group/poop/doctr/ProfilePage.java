56dd7f01d7f0178d98c16c47b0d246f15a9f9f30
DESKTOP-IKCSH5V\mosborn1987 <mosborn1987@gmail.com>
2017-11-09 18:41:11 -0500

    wokdjsf

--- app/src/main/java/com/group/poop/doctr/ProfilePage.java
+++ app/src/main/java/com/group/poop/doctr/ProfilePage.java
@@ -1,5 +1,6 @@
 package com.group.poop.doctr;
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -19,11 +21,67 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class ProfilePage extends AppCompatActivity {
+    // Profile Image
+    private ImageButton profilePictureImageButton;
+
+    // Profile Info
+    private TextView userNameTextView;
+    private TextView dateOfBirthTextView;
+    private TextView emailAddressTextView;
+    private TextView addressTextView;
+
+    // Widgets
+    private ImageButton settingsImageButton;
+    private ImageButton emailImageButton;
+    private ImageButton calendarImageButton;
+    private ImageButton searchImageButton;
+    private ImageButton servicesImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilepage);
+
+        // Profile Image
+        profilePictureImageButton = (ImageButton) findViewById(R.id.profilePictureImageButton);
+
+        // Profile Info
+        userNameTextView = (TextView) findViewById(R.id.userNameTextView);
+        dateOfBirthTextView = (TextView) findViewById(R.id.dateOfBirthTextView);
+        emailAddressTextView = (TextView) findViewById(R.id.emailAddressTextView);
+        addressTextView = (TextView) findViewById(R.id.addressTextView);
+
+        // Widgets
+        settingsImageButton = (ImageButton) findViewById(R.id.settingsImageButton);
+        emailImageButton = (ImageButton) findViewById(R.id.emailImageButton);
+        calendarImageButton = (ImageButton) findViewById(R.id.calendarImageButton);
+        searchImageButton = (ImageButton) findViewById(R.id.searchImageButton);
+        servicesImageButton = (ImageButton) findViewById(R.id.servicesImageButton);
+
+    }
+
+    public void onClickSettingsButton(View view){
+        Intent intent = new Intent(this, SettingsPage.class);
+        startActivity(intent);
+
+    }
+    public void onClickEmailButton(View view){
+        Intent intent = new Intent(this, MessengerPage.class);
+        startActivity(intent);
+    }
+    public void onClickCalendarButton(View view){
+        Intent intent = new Intent(this, CalendarPage.class);
+        startActivity(intent);
+
+    }
+    public void onClickSearchButton(View view){
+        Intent intent = new Intent(this, SearchPage.class);
+        startActivity(intent);
+
+    }
+    public void onClickAddServicesButton(View view){
+        Intent intent = new Intent(this, AddServicesPage.class);
+        startActivity(intent);
     }
 
 }
