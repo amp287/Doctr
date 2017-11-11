c64278a49edec428bd49b8e2c465ada0e8b6deae
DESKTOP-IKCSH5V\mosborn1987 <mosborn1987@knights.ucf.edu>
2017-11-11 15:18:25 -0500

    Spinner

--- app/src/main/java/com/group/poop/doctr/AddServicesPage.java
+++ app/src/main/java/com/group/poop/doctr/AddServicesPage.java
@@ -21,14 +21,34 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class AddServicesPage extends AppCompatActivity {
+
+    private Spinner generalPracticeSpinner;
+    private TextView tempTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+
+        generalPracticeSpinner = (Spinner) findViewById(R.id.generalPracticeSpinner);
+        tempTextView = (TextView) findViewById(R.id.tempTextView);
+
     }
 
     public void onClickBackButton(View view){
         AddServicesPage.this.finish();
     }
+
+    public String getSpinnerSelectedStringValue(Spinner spinner)
+    {
+        return spinner.getSelectedItem().toString();
+    }
+
+    public void setTheSpecialitySpinner(String generalPractice)
+    {
+        
+    }
+
+
 }
 
