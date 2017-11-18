3c21d0ebada430022b306d7eab62e0729946bd83
Luke Myers <luke.myers@knights.ucf.edu>
2017-11-18 18:56:49 -0500

    Implemented sign out functionality so a signed in user can sign out.

--- app/src/main/java/com/group/poop/doctr/UserHome.java
+++ app/src/main/java/com/group/poop/doctr/UserHome.java
@@ -1,13 +1,19 @@
 package com.group.poop.doctr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserHome extends AppCompatActivity {
+
+    private Button mSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,15 @@ public class UserHome extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mSignOut = findViewById(R.id.signOut);
+        mSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(UserHome.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
