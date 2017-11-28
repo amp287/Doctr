a782ea3e50b6f05e821178fe860564dda9a78552
Jonathon Rice <jrice145@gmail.com>
2017-11-28 00:41:06 -0500

    Patient Profile Pic
    Patient now pulls down stored pic and sets as profile pic.

--- app/src/main/java/com/group/poop/doctr/UserHome.java
+++ app/src/main/java/com/group/poop/doctr/UserHome.java
@@ -1,5 +1,6 @@
 package com.group.poop.doctr;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -16,6 +18,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class UserHome extends AppCompatActivity {
 
@@ -24,6 +27,9 @@ public class UserHome extends AppCompatActivity {
     private Button mSelectImage;
     private StorageReference mStorage;
     private static final int GALLERY_INTENT = 2;
+
+    private ProgressDialog mProgress;
+    private ImageView mProfileView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,8 @@ public class UserHome extends AppCompatActivity {
         mStorage = FirebaseStorage.getInstance().getReference();
         mSelectImage = (Button) findViewById(R.id.selectImage);
 
+        mProgress = new ProgressDialog(this);
+
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,22 +61,36 @@ public class UserHome extends AppCompatActivity {
                 startActivityForResult(intent, GALLERY_INTENT);
             }
         });
+
+        mProfileView = (ImageView) findViewById(R.id.profileView);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Uri uri = data.getData();
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
 
-        StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+            mProgress.setMessage("Uploading Image");
+            mProgress.show();
 
-        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+            Uri uri = data.getData();
 
-                Toast.makeText(UserHome.this, "Upload Done", Toast.LENGTH_LONG).show();
-            }
-        });
+            StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    mProgress.dismiss();
+
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+
+                    Picasso.with(UserHome.this).load(downloadUri).fit().centerCrop().into(mProfileView);
+
+                    Toast.makeText(UserHome.this, "Upload Done", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
