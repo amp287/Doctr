7828a960a89139350bebfa948be207cab93df733
Jonathon Rice <jrice145@gmail.com>
2017-11-27 22:59:18 -0500

    Patient Upload Pics
    patient can upload profile pics.
    Use camera on emulator to create a pic  to upload.

--- app/src/main/java/com/group/poop/doctr/UserHome.java
+++ app/src/main/java/com/group/poop/doctr/UserHome.java
@@ -1,6 +1,7 @@
 package com.group.poop.doctr;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class UserHome extends AppCompatActivity {
 
     private Button mSignOut;
+
+    private Button mSelectImage;
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,34 @@ public class UserHome extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mSelectImage = (Button) findViewById(R.id.selectImage);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri uri = data.getData();
+
+        StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Toast.makeText(UserHome.this, "Upload Done", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
