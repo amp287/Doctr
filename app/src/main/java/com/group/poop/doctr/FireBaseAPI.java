a4d63cee8cc72fa2a17ab90d161da16ea3303caa
DESKTOP-IKCSH5V\mosborn1987 <mosborn1987@knights.ucf.edu>
2017-11-28 14:46:01 -0500

    sdf

--- app/src/main/java/com/group/poop/doctr/FireBaseAPI.java
+++ app/src/main/java/com/group/poop/doctr/FireBaseAPI.java
@@ -1,8 +1,5 @@
 package com.group.poop.doctr;
 
-import android.util.Log;
-
-import com.google.firebase.*;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,20 +16,12 @@ public class FireBaseAPI {
     private static DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
     public static Doctor doctor;
 
-    // Static initialization
-    static{
-        requestCurrentDoctor();
-    }
-
     public static void writeCurrentUser(User user)
     {
-        // TODO - This API still needs
-        //User tempUser = getCurrentUser();
-
+        // NOTE - This is not working yet (I don't think).
         String userId = FirebaseAuth.getInstance().getUid();
 
         // TODO - Resolve whether it is a dr or profile
-
         mDatabase.child("UserProfiles").child(userId).setValue(user);
         mDatabase.child("DoctorProfiles").child(userId).setValue(user);
 
@@ -40,17 +29,19 @@ public class FireBaseAPI {
 
     private static void requestCurrentDoctor()
     {
-        // TODO - An API needs to be created for interfacing with firebase.
         String uid = FirebaseAuth.getInstance().getUid();
         mDatabase.child("DoctorProfiles").child(uid).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 String doctorString = (String) snapshot.getValue().toString();
                 doctor = new Doctor(doctorString);
+                // TODO - Place your code here
+
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
+
         });
 
     }
