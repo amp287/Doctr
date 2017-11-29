6d074e18708aaa67863c123b9fdf0a1fe81b9cb0
meenakarthik <meenakshikarthikeya@gmail.com>
2017-11-29 03:23:43 -0500

    Patient Request now updates the Appointment in FB with PatientUID

--- app/src/main/java/com/group/poop/doctr/CreateAppointment.java
+++ app/src/main/java/com/group/poop/doctr/CreateAppointment.java
@@ -86,6 +86,7 @@ public class CreateAppointment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Create Appointments");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_appointment);
 
